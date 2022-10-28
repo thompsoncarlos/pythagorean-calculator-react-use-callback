99a0ea001549d6e34b2cd0d49b4a5a6e6ea50140
Carlos Thompson <ce-thompson@criticalsoftware.com>
2022-10-28 16:17:22 +0100

    Adding useCallback

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Calculator from './Calculator';
+import { useCallback } from 'react';
 
 function App() {
+
+  const squareRoot = Math.sqrt()
   
-  const hypotenuseCalc = (sideA, sideB) => {
+  const hypotenuseCalc = useCallback((sideA, sideB) => {
     const squaredA = Math.pow(sideA, 2)
     const squaredB = Math.pow(sideB, 2)
-    return Math.sqrt(squaredA + squaredB)
-  }
+    return squareRoot(squaredA + squaredB)
+  }, [squareRoot])
+
   return (
     <div className="App">
       <img style={{ maxWidth: '500px' }} src="images/rigth-triangle.png" alt="Rigth triangle"/>
